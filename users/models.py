07a6188592ac4f255c27ba77dcdc8f9537499880
Nazir <nazirsherzad12345@gmail.com>
2025-09-26 18:18:25 +0430

    first commit

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class User(models.Model):
+    header_sections = models.JSONField(default=list)
+    hero_image = models.ImageField(upload_to='images', null=True,blank=True)
+
+    def __str__(self):
+      return 'Header contet'
+  
