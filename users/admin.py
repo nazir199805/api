ea9d264756d829392780912dccb7c06a109ce200
Nazir <nazirsherzad12345@gmail.com>
2025-09-27 15:37:34 +0430

    Images added

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, offer
+from .models import User, offer, HeroImage, HeroButton
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(offer)
+admin.site.register(HeroImage)
+admin.site.register(HeroButton)
