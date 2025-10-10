c9151ef76d68e6a7a83e88535de191d9a87c1a2c
Nazir <nazirsherzad12345@gmail.com>
2025-10-10 21:16:46 +0430

    url

--- users/views.py
+++ users/views.py
@@ -113,7 +113,7 @@ class FacebookLogin(APIView):
         token_url = "https://graph.facebook.com/v19.0/oauth/access_token"
         params = {
             "client_id": settings.SOCIAL_AUTH_FACEBOOK_KEY,
-            "redirect_uri": "http://localhost:3000",  # Must match your frontend redirect
+            "redirect_uri": "https://tashya-mendez.onrender.com/auth/facebook/callback/",
             "client_secret": settings.FSOCIAL_AUTH_FACEBOOK_SECRET,
             "code": code,
         }
