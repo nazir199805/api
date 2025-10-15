3cacee9e28b296881b804f21c3ed2e434467c12f
Nazir <nazirsherzad12345@gmail.com>
2025-10-15 11:40:05 +0430

    email removed

--- Api/settings.py
+++ Api/settings.py
@@ -201,14 +201,15 @@ PAYPAL_BASE_URL = "https://api-m.sandbox.paypal.com"
 
 
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'resetdjango8@gmail.com'
-EMAIL_HOST_PASSWORD = 'lkbv usgq snba mziv'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'resetdjango8@gmail.com' 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'resetdjango8@gmail.com'
+# EMAIL_HOST_PASSWORD = 'lkbv usgq snba mziv'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = 'resetdjango8@gmail.com' 
 
 
 ACCOUNT_LOGIN_METHODS = ["email"]
