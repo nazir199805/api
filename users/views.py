d8c086a345a480bc8ace430fa604ab5fbcbf87ec
Nazir <nazirsherzad12345@gmail.com>
2025-10-15 10:37:53 +0430

    slash added

--- users/views.py
+++ users/views.py
@@ -37,7 +37,7 @@ class GoogleCodeExchangeView(LoginView, APIView):
             "code": code,
             "client_id": settings.GOOGLE_CLIENT_ID,
             "client_secret": settings.GOOGLE_CLIENT_SECRET,
-            "redirect_uri": "https://aboutyouwebsite.vercel.app/callback/",
+            "redirect_uri": "https://aboutyouwebsite.vercel.app/",
             "grant_type": "authorization_code",
         }
 
