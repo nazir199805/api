ee06487579372a62c3c163b8b597ed99cdad39c4
Nazir <nazirsherzad12345@gmail.com>
2026-07-08 22:04:20 +0430

    hero

--- Api/settings.py
+++ Api/settings.py
@@ -1,10 +1,10 @@
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 from pathlib import Path
 from datetime import timedelta
 from django.utils.translation import gettext_lazy as _
 
-# load_dotenv()
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
